535a00c26d3f72ac7aa7bf094de36cd05329a0db
Sultan-Lodhi <78294923+Sultan-Lodhi@users.noreply.github.com>
2024-06-05 01:32:17 +0530

    Added files to src/string-calculator folder

--- src/string-calculator/add.test.ts
+++ src/string-calculator/add.test.ts
@@ -5,4 +5,28 @@ describe('add should', () => {
   test('return 0 when given string is empty', () => {
     expect(add('')).toEqual(0);
   });
+  test('return the number itself when given string contains one number', () => {
+    expect(add('1')).toEqual(1);
+  });
+  test('return the sum of two numbers when given string contains two numbers', () => {
+    expect(add('1,2')).toEqual(3);
+  });
+  test('return the sum of three numbers when given string contains three numbers', () => {
+    expect(add('1,2,3')).toEqual(6);
+  });
+  test('return the sum of four numbers when given string contains four numbers', () => {
+    expect(add('1,2,3,4')).toEqual(10);
+  });
+  test('return the sum of numbers when given string contains one new line', () => {
+    expect(add('1,2\n3')).toEqual(6);
+  });
+  test('return the sum of numbers when given string contains two new line', () => {
+    expect(add('1,2\n3\n4')).toEqual(10);
+  });
+  test('return the sum of numbers when given string contains new delimiter', () => {
+    expect(add('//;\n1;2')).toEqual(3);
+  });
+  test('throw an exception when given string contains negative number(s)', () => {
+    expect(() => add('1,-2,-3')).toThrow('negative numbers not allowed -2,-3');
+  });
 });
--- src/string-calculator/add.ts
+++ src/string-calculator/add.ts
@@ -1,1 +1,25 @@
-export const add = (numbers: string): number => 0;
+export const add = (numbers: string): number => {
+  let sum = 0;
+  let delimiter = ',';
+  let newLine = '\n';
+  let newDelimter = '//';
+  let negativeNumbers: string[] = [];
+
+  const lines: string[] = numbers.split(newLine);
+
+  for (const line of lines) {
+    if (line.substring(0, 2) === newDelimter) {
+      delimiter = line[2];
+      continue;
+    }
+    const values: string[] = line.split(delimiter);
+    for (const val of values) {
+      if (Number(val) > 0) sum += Number(val);
+      else val && negativeNumbers.push(val);
+    }
+  }
+
+  if (negativeNumbers.length) throw new Error(`negative numbers not allowed ${negativeNumbers.join(',')}`);
+
+  return sum;
+};
